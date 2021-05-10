f2b4886b72c6b2590a225a518ffcd7366d2a63f3
Grayson Hicks <graysonhicks@gmail.com>
2021-05-10 16:43:48 -0400

    bump to next

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
+    `gatsby-plugin-loadable-components-ssr`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {

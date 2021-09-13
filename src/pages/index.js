2adc9eb7d3e533c068b502b3625d500f9493052d
Grayson Hicks <graysonhicks@gmail.com>
2021-09-13 15:46:37 -0400

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Seo from "../components/seo"
 const Layout = React.lazy(() => import("../components/layout"))
 
 const IndexPage = () => (
+<<<<<<< Updated upstream
   <>
     <React.Suspense fallback={"Loading..."}>
       <Layout>
@@ -28,6 +29,28 @@ const IndexPage = () => (
       </Layout>
     </React.Suspense>
   </>
+=======
+  <React.Suspense fallback="Loading...">
+    <Layout>
+      <Seo title="Home" />
+      <h1>Hi Test</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <StaticImage
+        src="../images/gatsby-astronaut.png"
+        width={300}
+        quality={95}
+        formats={["AUTO", "WEBP", "AVIF"]}
+        alt="A Gatsby astronaut"
+        style={{ marginBottom: `1.45rem` }}
+      />
+      <p>
+        <Link to="/page-2/">Go to page 2</Link> <br />
+        <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+      </p>
+    </Layout>
+  </React.Suspense>
+>>>>>>> Stashed changes
 )
 
 export default IndexPage
--- src/templates/load-test.js
+++ src/templates/load-test.js
@@ -6,15 +6,16 @@ const Text = React.lazy(() => import("../components/text"))
 
 export default function LoadTest({ pageContext }) {
   return (
-    <div>
-      <h1 style={{ textAlign: "center", marginTop: "30px" }}>
-        Load Test {pageContext.index}
-      </h1>
-      <React.Suspense fallback={"Loading..."}>
+    <React.Suspense fallback="Loading...">
+      <div>
+        <h1 style={{ textAlign: "center", marginTop: "30px" }}>
+          Load Test {pageContext.index}
+        </h1>
+
         {pageContext.hasHeading && <Heading>Yep, I have a heading!</Heading>}
         {pageContext.hasText && <Text>Yep, I have text!</Text>}
         {pageContext.hasCarousel && <Carousel />}
-      </React.Suspense>
-    </div>
+      </div>
+    </React.Suspense>
   )
 }
